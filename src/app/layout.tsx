d9891cab3a60e8b2f1eac77f62e442fe3444acbf
Jeff <jeff2266@gmail.com>
2023-07-09 00:52:41 -0700

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Rubik } from 'next/font/google'
-import ClientProvider from '@/components/clientProvider'
+// import ClientProvider from '@/components/clientProvider'
 
 const rubik = Rubik({ subsets: ['latin'] })
 
