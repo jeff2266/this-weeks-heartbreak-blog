916a9a7e60fa2f02351cc1a210f2a8c4d3550983
Jeff <jeff2266@gmail.com>
2023-07-15 17:20:17 -0700

    Format signin signout pages

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -8,7 +8,7 @@ export default async function SignIn() {
 
 	return (
 		<fieldset className="flex flex-col items-center justify-center w-1/2 h-1/3 bg-black border px-4 pb-8 pt-12">
-			<legend className="p-1">
+			<legend className="p-1 text-center">
 				<h2>Sign In</h2>
 			</legend>
 			{providers?.google && <ProviderSignInButton provider={providers.google} image={googleSignIn} />}
--- src/app/auth/signout/page.tsx
+++ src/app/auth/signout/page.tsx
@@ -3,7 +3,10 @@ import SignOutButton from '@/components/signOutButton'
 export default async function SignOut() {
 	return (
 		<fieldset className="flex flex-col items-center justify-center w-1/2 h-1/3 bg-black border px-4 py-8">
-			<h2 className="my-4">Are you sure you want to sign out?</h2>
+			<legend className="p-1 text-center">
+				<h2>Sign Out</h2>
+			</legend>
+			<h2 className="my-4 text-center">Are you sure you want to sign out?</h2>
 			<SignOutButton />
 		</fieldset>
 	)
