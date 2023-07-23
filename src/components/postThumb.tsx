63464c123205117148b88793a22d9d56f595dbb8
Jeff <jeff2266@gmail.com>
2023-07-23 09:59:12 -0700

    change a to next Link

--- src/components/postThumb.tsx
+++ src/components/postThumb.tsx
@@ -1,5 +1,6 @@
 import { StaticImageData } from 'next/image'
 import PostThumbImage from './postThumbImage'
+import Link from 'next/link'
 
 type Params = {
 	id: string
@@ -16,13 +17,13 @@ export default function PostThumb({ post: { id, title, authorName, date, thumbUr
 			<div className="border-2 rounded-sm p-2">
 				<div className="flex flex-col">
 					<PostThumbImage params={{ id, title, thumbUrl, mediaUrl }} />
-					<a href={`posts/${id}`} className="mt-1">
+					<Link href={`posts/${id}`} className="mt-1">
 						<h3>{title}</h3>
 						<div className="flex justify-between text-sm">
 							<p>{authorName}</p>
 							<p>{new Date(date).toLocaleDateString('en-US', { dateStyle: 'short' })}</p>
 						</div>
-					</a>
+					</Link>
 				</div>
 			</div>
 		</div>
