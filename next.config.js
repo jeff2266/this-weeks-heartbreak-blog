528eb2185575aa11b4825acf96c3b023c7eda96d
Jeff <jeff2266@gmail.com>
2023-08-08 00:55:48 -0700

    .

--- next.config.js
+++ next.config.js
@@ -17,12 +17,12 @@ const nextConfig = {
 	async headers() {
 		const url = process.env.VERCEL_ENV ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
 		return [{
-			source: '/api/*',
+			source: '/api/:path*',
 			headers: [
-				{ key: "Access-Control-Allow-Credentials", value: "true" },
-				{ key: "Access-Control-Allow-Origin", value: `${url}*` },
-				{ key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-				{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+				{ key: 'Access-Control-Allow-Credentials', value: 'true' },
+				{ key: 'Access-Control-Allow-Origin', value: `${url}*` },
+				{ key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+				{ key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' }
 			]
 		}]
 	}
