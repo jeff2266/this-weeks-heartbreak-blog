4c0fc8c0012693739c76e2c25542fc56e3b16d44
Jeff <jeff2266@gmail.com>
2023-08-08 01:00:30 -0700

    fix signout callback url

--- src/components/signOutButton.tsx
+++ src/components/signOutButton.tsx
@@ -1,14 +1,16 @@
 'use client'
 
 import { signOut } from 'next-auth/react'
+import { useClientContext } from './clientContext'
 
 export default function SignOutButton() {
+	const { baseUrl } = useClientContext()
 	return (
 		<button
 			className="group w-5/6 max-w-md border min-w-max border-slate-400 rounded-md bg-white hover:bg-slate-100 shadow-[inset_-2px_1px_rgba(8,48,255,0.3)] text-black px-2 py-1 m-2"
 			onClick={() =>
 				signOut({
-					callbackUrl: '/'
+					callbackUrl: baseUrl
 				})
 			}>
 			<div className="flex justify-center p-1">
