1268b45f4ade44ef2f47e850024593c9140c3b9f
Jeff <jeff2266@gmail.com>
2023-08-08 00:59:26 -0700

    fix signin callback url

--- src/components/providerSignInButton.tsx
+++ src/components/providerSignInButton.tsx
@@ -1,14 +1,16 @@
 'use client'
 
 import { ClientSafeProvider, signIn } from 'next-auth/react'
+import { useClientContext } from './clientContext'
 import Image from 'next/image'
 
 export default function ProviderSignInButton({ provider, image }: { provider: ClientSafeProvider; image?: string }) {
+	const { baseUrl } = useClientContext()
 	return (
 		<button
 			className="group w-5/6 max-w-md border min-w-max border-slate-400 rounded-md bg-white hover:bg-slate-100 shadow-[inset_-2px_1px_rgba(8,48,255,0.3)] text-black px-2 py-1 m-2"
 			key={provider.id}
-			onClick={() => signIn(provider.id, { callbackUrl: '/' })}>
+			onClick={() => signIn(provider.id, { callbackUrl: baseUrl })}>
 			<div className="flex justify-center p-1">
 				<div className="relative w-9 p-4">
 					{image && <Image src={image} alt="provider icon" fill={true} style={{ objectFit: 'contain' }} />}
