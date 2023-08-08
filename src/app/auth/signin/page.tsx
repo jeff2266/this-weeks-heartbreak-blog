e0dcce38d1a893cc9cd02e041160be6591d199e7
Jeff <jeff2266@gmail.com>
2023-08-08 00:36:24 -0700

    .

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -6,6 +6,8 @@ export default async function SignIn() {
 	const googleSignIn: string | undefined = (providers?.google && (await import('public/img/googleSignIn.svg')))?.default
 		.src
 
+	console.log(process.env.NEXTAUTH_URL)
+	console.log(providers)
 	return (
 		<fieldset className="flex flex-col items-center justify-center w-2/3 md:w-1/2 h-1/3 bg-black border px-4 pb-8 pt-12">
 			<legend className="p-1 text-center">
