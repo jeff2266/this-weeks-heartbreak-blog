7194b1c9dbebadd2834a16bd80e9853f9161e891
Jeff <jeff2266@gmail.com>
2023-08-07 21:36:11 -0700

    Update middleware to secure api routes

---
+++ src/app/(blog)/author/posts/page.tsx
@@ -0,0 +1,27 @@
+import SearchBar from '@/components/searchBar'
+import StaticTitle from '@/components/banner/staticTitle'
+import UserSignIn from '@/components/userSignIn'
+import HamburgerMenu from '@/components/hamburgerMenu'
+import PostsList from '@/components/postsList'
+
+const POSTS_PER_PAGE = 16
+
+export default async function AuthoredPosts() {
+	return (
+		<>
+			<div className="flex justify-center lg:justify-between">
+				<StaticTitle animate={true} />
+				<nav className="hidden lg:flex items-center justify-end">
+					<SearchBar />
+					<UserSignIn />
+				</nav>
+			</div>
+			<HamburgerMenu />
+			<div className="flex w-full justify-center">
+				<h2 className="my-4">Liked Posts</h2>
+			</div>
+			<PostsList type="LIKES" take={POSTS_PER_PAGE} />
+			<div className="h-16"></div>
+		</>
+	)
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,13 +4,23 @@ import { withAuth } from 'next-auth/middleware'
 export default withAuth({
 	callbacks: {
 		authorized: ({ token, req }) => {
-			return req.nextUrl.pathname === '/author'
+			return req.nextUrl.pathname.startsWith('/author')
 				? token?.role === Role.ADMIN || token?.role === Role.AUTHOR
-				: req.nextUrl.pathname === '/likes'
+				: req.nextUrl.pathname.startsWith('/likes')
 				? !!token
+				: req.nextUrl.pathname.startsWith('/api/like')
+				? !!token
+				: req.nextUrl.pathname.startsWith('/api/post')
+				? req.nextUrl.pathname.startsWith('/api/post/presign')
+					? true
+					: req.method === 'GET'
+					? req.nextUrl.searchParams.get('liked') === 'true'
+						? !!token
+						: true
+					: req.method === 'POST'
+					? token?.role === Role.ADMIN || token?.role === Role.AUTHOR
+					: true
 				: true
 		}
 	}
 })
-
-export const config = { matcher: ['/author', '/likes'] }
